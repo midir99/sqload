d6d2e24e47f423559c864a7699349f2b4424da9e
midir99 <midir99@proton.me>
2022-11-06 09:35:59 -0600

    Update documentation to match the new API functions

--- sqload.go
+++ sqload.go
@@ -1,4 +1,56 @@
-// Package sqload provides functions to load SQL code from strings or .sql files into tagged struct fields.
+// Package sqload provides functions to load SQL code from strings or .sql files into
+// tagged struct fields.
+//
+// Its usage is very straightforward; let's suppose you have the following SQL file:
+//
+// File queries.sql:
+//	-- query: FindUserById
+//	SELECT first_name,
+//	       last_name,
+//	       dob,
+//	       email
+//	  FROM user
+//	 WHERE id = :id;
+//
+//	-- query: UpdateFirstNameById
+//	UPDATE user
+//	   SET first_name = 'Ernesto'
+//	 WHERE id = :id;
+//
+//	-- query: DeleteUserById
+//	DELETE FROM user
+//	      WHERE id = :id;
+// You could load the SQL code of those queries into strings using the following:
+//
+// File main.go:
+//	package main
+//
+//	import (
+//		_ "embed"
+//		"fmt"
+//
+//		"github.com/midir99/sqload"
+//	)
+//
+//	//go:embed queries.sql
+//	var sqlCode string
+//
+//	var Q = sqload.MustLoadFromString[struct {
+//		FindUserById        string `query:"FindUserById"`
+//		UpdateFirstNameById string `query:"UpdateFirstNameById"`
+//		DeleteUserById      string `query:"DeleteUserById"`
+//	}](sqlCode)
+//
+//	func main() {
+//		fmt.Printf("- FindUserById\n%s\n\n", Q.FindUserById)
+//		fmt.Printf("- UpdateFirstNameById\n%s\n\n", Q.UpdateFirstNameById)
+//		fmt.Printf("- DeleteUserById\n%s\n\n", Q.DeleteUserById)
+//	}
+// The module maps the fields of your struct and the queries from the SQL file using the
+// query tag (in the struct):
+//	`query:"NameOfYourQuery"`
+// And the query comment (in the SQL code):
+//	-- query: NameOfYourQuery
 package sqload
 
 import (
@@ -10,6 +62,10 @@ import (
 	"regexp"
 	"strings"
 )
+
+// Struct is an empty interface used to give the developer a hint that the type must be
+// a struct.
+type Struct interface{}
 
 var queryNamePattern = regexp.MustCompile(`[ \t\n\r\f\v]*-- query:`)
 var validQueryNamePattern = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
@@ -60,7 +116,7 @@ func findFilesWithExt(fsys fs.FS, ext string) ([]string, error) {
 	return files, nil
 }
 
-func loadQueriesIntoStruct(queries map[string]string, v any) error {
+func loadQueriesIntoStruct(queries map[string]string, v Struct) error {
 	value := reflect.ValueOf(v)
 	if value.Kind() != reflect.Pointer {
 		return fmt.Errorf("v is not a pointer to a struct")
@@ -106,10 +162,52 @@ func cat(fsys fs.FS, filenames []string) (string, error) {
 	return txt, nil
 }
 
-// LoadFromString loads the SQL code from the string passed and stores the queries in the
-// struct pointed to by v, v must be a pointer to a struct with tags, and each tag
-// indicates what query will be stored in what field.
-func LoadFromString[V any](s string) (*V, error) {
+// LoadFromString loads the SQL code from the string and returns a pointer to a struct.
+// Each struct field will contain the SQL query code it was tagged with.
+//
+// If some query has an invalid name in the string or is not found in the string, it
+// will return a nil pointer and an error.
+//	package main
+//
+//	import (
+//		"fmt"
+//		"os"
+//
+//		"github.com/midir99/sqload"
+//	)
+//
+//	func main() {
+//		q, err := sqload.LoadFromString[struct {
+//			FindUserById        string `query:"FindUserById"`
+//			UpdateFirstNameById string `query:"UpdateFirstNameById"`
+//			DeleteUserById      string `query:"DeleteUserById"`
+//		}](`
+//	-- query: FindUserById
+//	SELECT first_name,
+//	       last_name,
+//	       dob,
+//	       email
+//	  FROM user
+//	 WHERE id = :id;
+//
+//	-- query: UpdateFirstNameById
+//	UPDATE user
+//	   SET first_name = 'Ernesto'
+//	 WHERE id = :id;
+//
+//	-- query: DeleteUserById
+//	DELETE FROM user
+//	      WHERE id = :id;
+//		`)
+//		if err != nil {
+//			fmt.Printf("Unable to load SQL queries: %s\n", err)
+//			os.Exit(1)
+//		}
+//		fmt.Printf("- FindUserById\n%s\n\n", q.FindUserById)
+//		fmt.Printf("- UpdateFirstNameById\n%s\n\n", q.UpdateFirstNameById)
+//		fmt.Printf("- DeleteUserById\n%s\n\n", q.DeleteUserById)
+//	}
+func LoadFromString[V Struct](s string) (*V, error) {
 	var v V
 	queries, err := extractQueries(s)
 	if err != nil {
@@ -122,7 +220,10 @@ func LoadFromString[V any](s string) (*V, error) {
 	return &v, nil
 }
 
-func MustLoadFromString[V any](s string) *V {
+// MustLoadFromString is like LoadFromString but panics if any error occurs. It
+// simplifies the safe initialization of global variables holding struct pointers
+// containing SQL queries.
+func MustLoadFromString[V Struct](s string) *V {
 	v, err := LoadFromString[V](s)
 	if err != nil {
 		panic(err)
@@ -130,10 +231,58 @@ func MustLoadFromString[V any](s string) *V {
 	return v
 }
 
-// LoadFromFile loads the SQL code from the file filename and stores the queries in the struct
-// pointed to by v, v must be a pointer to a struct with tags, and each tag indicates
-// what query will be stored in what field.
-func LoadFromFile[V any](filename string) (*V, error) {
+// LoadFromFile loads the SQL code from the file filename and returns a pointer to a
+// struct. Each struct field will contain the SQL query code it was tagged with.
+//
+// If some query has an invalid name in the string or is not found in the string, it
+// will return a nil pointer and an error.
+//
+// If the file can not be read or does not exist, it will return a nil pointer and an
+// error.
+//
+// File queries.sql:
+//	-- query: FindUserById
+//	SELECT first_name,
+//	       last_name,
+//	       dob,
+//	       email
+//	  FROM user
+//	 WHERE id = :id;
+//
+//	-- query: UpdateFirstNameById
+//	UPDATE user
+//	   SET first_name = 'Ernesto'
+//	 WHERE id = :id;
+//
+//	-- query: DeleteUserById
+//	DELETE FROM user
+//	      WHERE id = :id;
+//
+// File main.go:
+//	package main
+//
+//	import (
+//		"fmt"
+//		"os"
+//
+//		"github.com/midir99/sqload"
+//	)
+//
+//	func main() {
+//		q, err := sqload.LoadFromFile[struct {
+//			FindUserById        string `query:"FindUserById"`
+//			UpdateFirstNameById string `query:"UpdateFirstNameById"`
+//			DeleteUserById      string `query:"DeleteUserById"`
+//		}]("queries.sql")
+//		if err != nil {
+//			fmt.Printf("Unable to load SQL queries: %s\n", err)
+//			os.Exit(1)
+//		}
+//		fmt.Printf("- FindUserById\n%s\n\n", q.FindUserById)
+//		fmt.Printf("- UpdateFirstNameById\n%s\n\n", q.UpdateFirstNameById)
+//		fmt.Printf("- DeleteUserById\n%s\n\n", q.DeleteUserById)
+//	}
+func LoadFromFile[V Struct](filename string) (*V, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -141,7 +290,10 @@ func LoadFromFile[V any](filename string) (*V, error) {
 	return LoadFromString[V](string(data))
 }
 
-func MustLoadFromFile[V any](filename string) *V {
+// MustLoadFromFile is like LoadFromFile but panics if any error occurs. It simplifies
+// the safe initialization of global variables holding struct pointers containing SQL
+// queries.
+func MustLoadFromFile[V Struct](filename string) *V {
 	v, err := LoadFromFile[V](filename)
 	if err != nil {
 		panic(err)
@@ -150,10 +302,53 @@ func MustLoadFromFile[V any](filename string) *V {
 }
 
 // LoadFromDir loads the SQL code from all the .sql files in the directory dirname
-// (recursively) and stores the queries in the struct pointed to by v, v must be a
-// pointer to a struct with tags, and each tag indicates what query will be stored in
-// what field.
-func LoadFromDir[V any](dirname string) (*V, error) {
+// (recursively) and returns a pointer to a struct. Each struct field will contain the
+// SQL query code it was tagged with.
+//
+// If some query has an invalid name in the string or is not found in the string, it
+// will return a nil pointer and an error.
+//
+// If the directory can not be read or does not exist, it will return a nil pointer and
+// an error.
+//
+// If any .sql file can not be read, it will return a nil pointer and an error.
+//
+// Project directory:
+//	.
+//	├── go.mod
+//	├── main.go
+//	└── sql
+//	    ├── cats.sql
+//	    └── users.sql
+// File sql/cats.sql:
+//	-- query: CreatePsychoCat
+//	INSERT INTO Cat (name, color) VALUES ('Puca', 'Orange');
+// File sql/users.sql
+//	-- query: DeleteUserById
+//	DELETE FROM user WHERE id = :id;
+// File main.go:
+//	package main
+//
+//	import (
+//		"fmt"
+//		"os"
+//
+//		"github.com/midir99/sqload"
+//	)
+//
+//	func main() {
+//		q, err := sqload.LoadFromDir[struct {
+//			CreatePsychoCat string `query:"CreatePsychoCat"`
+//			DeleteUserById  string `query:"DeleteUserById"`
+//		}]("sql")
+//		if err != nil {
+//			fmt.Printf("Unable to load SQL queries: %s\n", err)
+//			os.Exit(1)
+//		}
+//		fmt.Printf("- CreatePsychoCat\n%s\n\n", q.CreatePsychoCat)
+//		fmt.Printf("- DeleteUserById\n%s\n\n", q.DeleteUserById)
+//	}
+func LoadFromDir[V Struct](dirname string) (*V, error) {
 	fsys := os.DirFS(dirname)
 	files, err := findFilesWithExt(fsys, ".sql")
 	if err != nil {
@@ -166,7 +361,10 @@ func LoadFromDir[V any](dirname string) (*V, error) {
 	return LoadFromString[V](sql)
 }
 
-func MustLoadFromDir[V any](dirname string) *V {
+// MustLoadFromDir is like LoadFromDir but panics if any error occurs. It simplifies the
+// safe initialization of global variables holding struct pointers containing SQL
+// queries.
+func MustLoadFromDir[V Struct](dirname string) *V {
 	v, err := LoadFromDir[V](dirname)
 	if err != nil {
 		panic(err)
@@ -174,7 +372,58 @@ func MustLoadFromDir[V any](dirname string) *V {
 	return v
 }
 
-func LoadFromFS[V any](fsys fs.FS) (*V, error) {
+// LoadFromFS loads the SQL code from all the .sql files in the fsys file system
+// (recursively) and returns a pointer to a struct. Each struct field will contain the
+// SQL query code it was tagged with.
+//
+// If some query has an invalid name in the string or is not found in the string, it
+// will return a nil pointer and an error.
+//
+// If the fsys can not be read or does not exist, it will return a nil pointer and
+// an error.
+//
+// If any .sql file can not be read, it will return a nil pointer and an error.
+//
+// Project directory:
+//	.
+//	├── go.mod
+//	├── main.go
+//	└── sql
+//	    ├── cats.sql
+//	    └── users.sql
+// File sql/cats.sql:
+//	-- query: CreatePsychoCat
+//	INSERT INTO Cat (name, color) VALUES ('Puca', 'Orange');
+// File sql/users.sql
+//	-- query: DeleteUserById
+//	DELETE FROM user WHERE id = :id;
+// File main.go:
+//	package main
+//
+//	import (
+//		"embed"
+//		"fmt"
+//		"os"
+//
+//		"github.com/midir99/sqload"
+//	)
+//
+//	//go:embed sql/*.sql
+//	var fsys embed.FS
+//
+//	func main() {
+//		q, err := sqload.LoadFromFS[struct {
+//			CreatePsychoCat string `query:"CreatePsychoCat"`
+//			DeleteUserById  string `query:"DeleteUserById"`
+//		}](fsys)
+//		if err != nil {
+//			fmt.Printf("Unable to load SQL queries: %s\n", err)
+//			os.Exit(1)
+//		}
+//		fmt.Printf("- CreatePsychoCat\n%s\n\n", q.CreatePsychoCat)
+//		fmt.Printf("- DeleteUserById\n%s\n\n", q.DeleteUserById)
+//	}
+func LoadFromFS[V Struct](fsys fs.FS) (*V, error) {
 	files, err := findFilesWithExt(fsys, ".sql")
 	if err != nil {
 		return nil, err
@@ -186,7 +435,10 @@ func LoadFromFS[V any](fsys fs.FS) (*V, error) {
 	return LoadFromString[V](sql)
 }
 
-func MustLoadFromFS[V any](fsys fs.FS) *V {
+// MustLoadFromFS is like LoadFromFS but panics if any error occurs. It simplifies the
+// safe initialization of global variables holding struct pointers containing SQL
+// queries.
+func MustLoadFromFS[V Struct](fsys fs.FS) *V {
 	v, err := LoadFromFS[V](fsys)
 	if err != nil {
 		panic(err)
